65eb164653ab9205454a21f81c5a3828fb5963fb
P Selvaraj <selvaraj.p88@gmail.com>
2025-07-03 21:44:12 +0530

    -m added

--- src/components/SuceessPage.jsx
+++ src/components/SuceessPage.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import '../styles/styles.css'; // if you're using external CSS
+import '../styles/styles.css'; // Import your CSS file
 
-export default function SuccessPage({ email }) {
+export default function Success({ email }) {
   return (
     <div className="sign-up-form">
       <img src="/images/icon-success.svg" alt="Success Icon" />
-      
+
       <h1 className="thanks-for-sub">Thanks for Subscribing!</h1>
-      
+
       <p className="success-text">
-        A confirmation email has been sent to <strong>{email}</strong>. Please open it and click the button inside to confirm your subscription.
+        A confirmation email has been sent to <strong>{email}</strong>.
+        Please open it and click the button inside to confirm your subscription.
       </p>
-      
+
       <button
         className="dismiss-btn"
         onClick={() => (window.location.href = 'index.html')}
